932eb3bebb0399b6aa52063e6d65fe59cac697fe
Abhirup <abhidatta146@gmail.com>
2022-07-07 21:37:11 +0530

    installing webview and source prop

--- App.js
+++ App.js
@@ -3,12 +3,20 @@ import {
   View,
   Text,
 } from 'react-native';
+import {WebView} from 'react-native-webview';
 
+const WebviewDemo = ()=>{
+  return (
+    <WebView source={{uri: "https://hackernews.com"}}/>
+  )
+}
 
 const App = () => {
   return (
-    <View>
-      <Text>Hello </Text>
+    <View style={{flex: 1}}>
+      <Text style={{height: 200}}>Hello World </Text>
+      <WebviewDemo />
+      <Text style={{height: 200}}>Other Text</Text>
     </View>
   );
 };
