56b0036931e9ddc28e3349295727ae2d0b310257
Abhirup <abhidatta146@gmail.com>
2022-07-09 11:12:11 +0530

    customHTMl and injectJavscript props

--- App.js
+++ App.js
@@ -1,13 +1,45 @@
-import React from 'react';
+import React, {useEffect, useRef} from 'react';
 import {
   View,
   Text,
 } from 'react-native';
 import {WebView} from 'react-native-webview';
 
+// const WebviewDemo = ()=>{
+//   return (
+//     <WebView source={{uri: "https://hackernews.com"}}/>
+//   )
+// }
+
 const WebviewDemo = ()=>{
+  const webRef = useRef();
+  const runFirst = `
+    setTimeout(function(){
+      window.alert('Click me');
+      document.getElementById('h2_element').innerHTML = 'Hello';
+    }, 1000);
+  `;
+
+  const run = `
+    document.getElementById('h2_element').innerHTML = 'World';
+  `;
+
+  useEffect(()=>{
+    setTimeout(()=>{
+     webRef.current.injectJavaScript(run);
+    },5000);
+  },[]);
+
+  const customHtml = `<body style="display:flex; flex-direction: column; justify-content: center; align-items:center;background-color: black;color: white">
+  <h1 style="font-size: 100px; padding: 50px; text-align: center">This is simple html</h1>
+  <h2 style="display: block; font-size:80px; padding: 50px; 
+          text-align: center;" id="h2_element">
+            This text will be changed later!
+          </h2>
+  </body>`;
+
   return (
-    <WebView source={{uri: "https://hackernews.com"}}/>
+    <WebView ref={webRef} source={{html: customHtml}} injectedJavaScript={runFirst}/>
   )
 }
 
--- Readme.md
+++ Readme.md
@@ -1,7 +1,3 @@
 ### What is webview ?
 WebView objects allow you to display web content as part of your activity layout, but lack some of the features of fully-developed browsers. 
 
-## How to video tutorial ?
-- What and why of react-native-webview
-- Go the npm library website(https://github.com/react-native-webview/react-native-webview) and go the "Getting started" guide.
-- Show how to install
